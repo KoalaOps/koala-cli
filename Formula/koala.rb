6528edd3dab073e099af43b0334e9bdacd11451c
Koala Ops <nadav@koalaops.com>
2023-07-06 17:04:29 +0300

    Brew formula update for koala-backend version 0.1.13

--- Formula/koala.rb
+++ Formula/koala.rb
@@ -5,11 +5,11 @@
 class Koala < Formula
   desc "CLI for Koala Ops."
   homepage "https://www.koalaops.com"
-  version "0.1.12"
+  version "0.1.13"
 
   on_macos do
-    url "https://github.com/KoalaOps/koala-cli/releases/download/0.1.12/koala_cli_Darwin_all"
-    sha256 "9a4994603debd348bb832776627934dcc9809b05df9f387af357a599f11fe70f"
+    url "https://github.com/KoalaOps/koala-cli/releases/download/0.1.13/koala_cli_Darwin_all"
+    sha256 "53b71436d167e8bc2bc1a2156b4eab198b845dffd2893a290d12c1b2d92ef791"
 
     def install
       bin.install "koala_cli_Darwin_all" => "koala"
@@ -17,20 +17,20 @@ class Koala < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/KoalaOps/koala-cli/releases/download/0.1.12/koala_cli_Linux_arm64"
-      sha256 "020ec8e1c2ec3085b6bd48ee64bd67eb3f9b9c1a16aaf1b82c0bc72b2e965adc"
-
-      def install
-        bin.install "koala_cli_Linux_arm64" => "koala"
-      end
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/KoalaOps/koala-cli/releases/download/0.1.12/koala_cli_Linux_x86_64"
-      sha256 "489a4a7d97bdffcb409d5f536a0a9311cb48faa21a3dac09a4678b02ef773052"
+      url "https://github.com/KoalaOps/koala-cli/releases/download/0.1.13/koala_cli_Linux_x86_64"
+      sha256 "37234cc4d4ac0828bd47e9b7c3605795936c1342ef90137d0f68ba066562b2f9"
 
       def install
         bin.install "koala_cli_Linux_x86_64" => "koala"
+      end
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/KoalaOps/koala-cli/releases/download/0.1.13/koala_cli_Linux_arm64"
+      sha256 "33d9a60ee85f2ecea1a0a982b3f48343546f8cd6f92573db7f25c334806e79c4"
+
+      def install
+        bin.install "koala_cli_Linux_arm64" => "koala"
       end
     end
   end
